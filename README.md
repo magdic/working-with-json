5aa13ab5205fdc50103eb90d7ed74eb8cfc6358e
Magdiel <luisc@thehangar.cr>
2014-08-07 15:15:30 -0600

    Working with json

---
+++ README.md
@@ -0,0 +1,15 @@
+Working with Json
+==========
+
+Calling facebook json with id or username and display a info about the user requested.
+
+- Live Website for Test: <a href="" target="_blank">Live Test | Github Pages</a>
+
+Instrucctions
+=============
+Under construction...
+
+## License
+Fell free to edit and share.
+
+#### :copyright: Magdiel Cordero
---
+++ js/script.js
@@ -0,0 +1,38 @@
+function getUrlVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+ var callUsername = getUrlVariable("taken");
+
+(function() {
+
+  var flickerAPI = "https://graph.facebook.com/"+callUsername;
+  $.getJSON( flickerAPI, {
+    format: "json"
+  })
+    .done(function( data ) {
+        console.log(data);
+
+        var userid = data.id;
+
+        $('#fullname').text(data.name);
+        $('#username').text(data.username);
+        $('#idU').text(data.id);
+        $('#gender').text(data.gender);
+        $('#locale').text(data.locale);
+        $('#about').text(data.about);
+
+        var imgPaht = "https://graph.facebook.com/"+userid+"/picture?width=200&height=200";
+
+        $('<img src="'+ imgPaht +'">').load(function() {
+          $(this).appendTo('#images');
+        })
+    });
+})();
+
